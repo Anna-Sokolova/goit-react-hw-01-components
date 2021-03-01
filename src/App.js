fa8ad482d3f880cffe6450fbc2396f3858fc9e7a
Anna Sokolova <sokolovaskorik@gmail.com>
2021-03-01 22:41:43 +0200

    Added react lint config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
-import Profile from "./components/Profile/Profile"; //импорт компонента профиля соц сети
-import Statistics from "./components/Statistics/Statistics"; // импорт компонента статистики
-import FriendList from "./components/FriendList/FriendList"; //// импорт компонента списка друзей
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory"; //импорт компонента транзакций
+import './App.css';
+import Profile from './components/Profile/Profile'; //импорт компонента профиля соц сети
+import Statistics from './components/Statistics/Statistics'; // импорт компонента статистики
+import FriendList from './components/FriendList/FriendList'; //// импорт компонента списка друзей
+import TransactionHistory from './components/TransactionHistory/TransactionHistory'; //импорт компонента транзакций
 
-import userProfile from "./db/user.json"; // импорт данных о пользователе компонента Profile
-import statisticData from "./db/statistical-data.json"; // импорт данных ститистики
-import friendsData from "./db/friends.json"; // импорт данных друзей
-import transactions from "./db/transactions.json"; //импорт данных транзакций
+import userProfile from './db/user.json'; // импорт данных о пользователе компонента Profile
+import statisticData from './db/statistical-data.json'; // импорт данных ститистики
+import friendsData from './db/friends.json'; // импорт данных друзей
+import transactions from './db/transactions.json'; //импорт данных транзакций
 
 const { name, tag, location, avatar, stats } = userProfile; //деструктуризация объекта данных
 
@@ -15,13 +15,19 @@ function App() {
   return (
     <div className="App">
       <h3>Профиль социальной сети</h3>
-      <Profile name={name} tag={tag} location={location} avatar={avatar} stats={stats} />
+      <Profile
+        name={name}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
       <h3>Секция статистики</h3>
       <Statistics title="" stats={statisticData} />
       <h3>Список друзей</h3>
-      <FriendList friends={friendsData}/>
+      <FriendList friends={friendsData} />
       <h3>История транзакций</h3>
-      <TransactionHistory items={transactions}/>
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
